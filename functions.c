817d46b278eb516b6e66a258845c1e0791f715ea
AMG1995 <39673621+AMG1995@users.noreply.github.com>
2023-09-19 14:52:04 +0400

    Update functions.c

--- functions.c
+++ functions.c
@@ -5,10 +5,10 @@
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * @width: the Width
+ * @precision: specification
+ * @size: Size of specifier
+ * Return: chars printed
  */
 int print_char(va_list types, char buffer[],
 int flags, int width, int precision, int size)
